396ccd1b1978cf565e3f190513c2c9436a6834e6
Patrick S. Vogt <patrick.vogt@unibas.ch>
2011-06-10 22:02:52 +0200

    correct log tag

--- src/ch/almana/unibas/rooms/helper/Logger.java
+++ src/ch/almana/unibas/rooms/helper/Logger.java
@@ -5,7 +5,7 @@ import android.util.Log;
 public class Logger {
 
 
-	private static String TAG = "perssearch";
+	private static String TAG = "rooms@unibas";
 
 
 	public static void e(String msg, Throwable t) {
