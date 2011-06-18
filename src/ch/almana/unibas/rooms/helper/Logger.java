b4ff4ffad1bc034d05c470c649b93a2d620c77fe
Patrick S. Vogt <patrick.vogt@unibas.ch>
2011-06-18 13:47:53 +0200

    new logtag

--- src/ch/almana/unibas/rooms/helper/Logger.java
+++ src/ch/almana/unibas/rooms/helper/Logger.java
@@ -5,7 +5,7 @@ import android.util.Log;
 public class Logger {
 
 
-	private static String TAG = "rooms@unibas";
+	private static String TAG = "roomsUnibas";
 
 
 	public static void e(String msg, Throwable t) {
