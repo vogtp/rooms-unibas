cc61e15ccd02ea7657b1e1694e2bffedf71bac2d
Patrick S. Vogt <patrick.vogt@unibas.ch>
2011-06-18 19:22:25 +0200

    display weekday

--- src/ch/almana/unibas/rooms/helper/Settings.java
+++ src/ch/almana/unibas/rooms/helper/Settings.java
@@ -3,7 +3,7 @@ package ch.almana.unibas.rooms.helper;
 import java.text.SimpleDateFormat;
 
 public class Settings {
-	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd.MM.yyyy");
+	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("EEE dd.MM.yyyy");
 	private static final SimpleDateFormat TIME_FORMAT = new SimpleDateFormat("HH:mm");
 
 	private static Settings instance;
